2691664875178ef7e9061ae0f270590c0ebcf7d6
Madalina Stanciu <madalinamazilu@gmail.com>
2018-12-18 00:12:14 +0200

    refactor: added pipeline and validate against naive implementation

--- src/main.py
+++ src/main.py
@@ -1,84 +1,69 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
-
-from scipy import signal
-from sklearn.decomposition import FastICA, PCA
-from sklearn.preprocessing import MinMaxScaler
 from datetime import tzinfo, timedelta, datetime
 
+from sklearn.decomposition import FastICA, PCA
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.pipeline import Pipeline
 
-from Pipeline import CustomPypeline
-from features.sources import FileDataSource, WebDataSource
-
+from features.sources import FileDataSource
 
 start_date = datetime(2010, 8, 3)
 end_date = datetime(2015, 5, 23)
+numberOfFeatures = 5
+colors = ['red', 'steelblue', 'orange', 'teal', 'green']
 
 
 dataSource = FileDataSource('data/ES 1 Day_Series_Indicators.csv')
-df = dataSource.Data()
-#df=ds.WebDataSource(start_date,end_date,'FB').Data()
-info = dataSource.GetMissingValueInfo(df)
-print(info)
+df = dataSource.dataFrame
+train_data, train_data_target = dataSource.GetData(), dataSource.Target
 
-# # load dataset
-# X, y = CustomPypeline.load_dataset()
-# # get model list
-# models = define_models()
+pipeline = Pipeline([
+    ('normalize', MinMaxScaler(feature_range=(-1, 1))),
+    ('featureExtraction', FastICA(
+        n_components=numberOfFeatures, fun='logcosh', whiten=True))
+])
 
-# # add gbm models
-# models = define_gbm_models(models)
+pipeline = pipeline.fit(train_data)
+S = pipeline.transform(train_data)
+
 
 # # evaluate models
-# results = evaluate_models(X, y, models)
+# results = CustomPipeline.evaluate_models(X, y, models, pipelines)
 # # summarize results
-# summarize_results(results)
+# CustomPipeline.summarize_results(results)
 
-X=df
-
-numberOfFeatures=5
-# normalize the data 
+X = df.values
+# normalize the data
 scaler = MinMaxScaler(feature_range=(-1, 1))
 scaler = scaler.fit(X)
-print('Min: {0}, Max: {1}'.format(scaler.data_min_, scaler.data_max_))
 # normalize the dataset and print the first 5 rows
 X = scaler.transform(X)
 
-ica = FastICA(n_components=numberOfFeatures,fun='logcosh',whiten=True)
+ica = FastICA(n_components=numberOfFeatures, fun='logcosh', whiten=True)
 S_ = ica.fit_transform(X)  # Reconstruct signals
-A_ = ica.mixing_  # Get estimated mixing matrix
-
-
-# For comparison, compute PCA
-pca = PCA(n_components=numberOfFeatures)
-H = pca.fit_transform(X)  # Reconstruct signals based on orthogonal components
 
 # #############################################################################
 # Plot results
 
-print(X.shape,S_.shape,H.shape)
-print(X[0:3,:])
-print(S_[0:3,:])
-print(H[0:3,:])
-
 df.plot()
 
 
-
-models = [X, S_, H]
 plt.figure()
-names = ['Observations (mixed signal)',
-         'ICA recovered signals',
-         'PCA recovered signals']
-colors = ['red', 'steelblue', 'orange','teal','green']
 
-for ii, (model, name) in enumerate(zip(models, names), 1):
-    plt.subplot(3, 1, ii)
-    plt.title(name)
-    for sig, color in zip(model.T, colors):
+signals = {
+    'Observations (mixed signal)': X,
+    'ICA recovered signals': S_,
+    'Pipeline fit signal': S
+}
+colors = ['red', 'steelblue', 'orange', 'teal', 'green']
+
+for idx, key in enumerate(signals, 1):
+
+    plt.subplot(len(signals), 1, idx)
+    plt.title(key)
+    for sig, color in zip(signals[key].T, colors):
         plt.plot(sig, color=color)
 
 plt.show()
-    
